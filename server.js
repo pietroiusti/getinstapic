2d1d57807e07a231676091c3c4c0da9ca1a86ded
G P <giulio.pietroiusti@gmail.com>
2018-03-31 18:29:36 +0200

    Don't bother with ip.

--- server.js
+++ server.js
@@ -2,107 +2,104 @@
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-const publicIp = require('public-ip');
 const mongodb = require('mongodb');
 
 const port = process.env.PORT || 3000;
 
-publicIp.v4().then(ip => {
-    // Connect to mongo db
-    mongodb.MongoClient.connect(process.env.DBURI, (err, client) => {
-	if (err) {
+// Connect to mongo db
+mongodb.MongoClient.connect(process.env.DBURI, (err, client) => {
+    if (err) {
+	console.log(err);
+    }
+
+    var collection = client.db('getinstapicdb').collection('usersRequests');
+
+    const server = http.createServer((req, res) => {
+	// Add error listener
+	req.on('error', (err) => {
 	    console.log(err);
-	}
+	    res.statusCode = 400;
+	    res.end();
+	});
 
-	var collection = client.db('getinstapicdb').collection('usersRequests');
-
-	const server = http.createServer((req, res) => {
-	    // Add error listener
-	    req.on('error', (err) => {
-		console.log(err);
-		res.statusCode = 400;
-		res.end();
-	    });
-
-	    if (req.method === 'GET') {
-		if (req.url === '/') {
-		    // Serve homepage
-		    fs.readFile('./public/homepage.html', (err, fileContent) => {
-			if (err) {
-			    console.log('Error 1');
-			} else {
-			    res.writeHead(200, {'Content-Type': 'text/html'});
-			    res.end(fileContent);
-			}
-		    });
-		} else if (req.url === '/public/css/styles.css') {
-		    fs.readFile('./public/css/styles.css', (err, fileContent) => {
-			if (err) {
-			    console.log('Error 2');
-			} else {
-			    res.writeHead(200, {'Content-Type': 'text/css'});
-			    res.end(fileContent);
-			}
-		    });
-		}else if (req.url === '/public/scripts/loadingGif.js') {
-		    fs.readFile('./public/scripts/loadingGif.js', (err, fileContent) => {
-			if (err) {
-			    console.log('Error 3');
-			} else {
-			    res.writeHead(200, {'Content-Type': 'text/javascript'});
-			    res.end(fileContent);
-			}
-		    });
-		} else if (req.url === '/public/images/lg.ajax-spinner-preloader.gif') {
-		    fs.readFile('./public/images/lg.ajax-spinner-preloader.gif', (err, fileContent) => {
-			if (err) {
-			    console.log('Error 4');
-			} else {
-			    res.writeHead(200, {'Content-Type': 'image/jpg'});
-			    res.end(fileContent);
-			}
-		    });	
-		} else {
-		    var splittedUrl = req.url.split('/');
-		    if (splittedUrl[1] === 'getpic') {
-			// Check user input
-			if (isInputLegal(getUserInput(req.url))) {
-			    // Find photo from link given, give it to user and store request in db.
-			    getPhoto(getUserInput(req.url), res, ip, collection, storeRequestInDb);
-			} else {
-			    res.statusCode = 404;
-			    res.end('Wrong link');			    
-			}
+	if (req.method === 'GET') {
+	    if (req.url === '/') {
+		// Serve homepage
+		fs.readFile('./public/homepage.html', (err, fileContent) => {
+		    if (err) {
+			console.log('Error 1');
+		    } else {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end(fileContent);
+		    }
+		});
+	    } else if (req.url === '/public/css/styles.css') {
+		fs.readFile('./public/css/styles.css', (err, fileContent) => {
+		    if (err) {
+			console.log('Error 2');
+		    } else {
+			res.writeHead(200, {'Content-Type': 'text/css'});
+			res.end(fileContent);
+		    }
+		});
+	    }else if (req.url === '/public/scripts/loadingGif.js') {
+		fs.readFile('./public/scripts/loadingGif.js', (err, fileContent) => {
+		    if (err) {
+			console.log('Error 3');
+		    } else {
+			res.writeHead(200, {'Content-Type': 'text/javascript'});
+			res.end(fileContent);
+		    }
+		});
+	    } else if (req.url === '/public/images/lg.ajax-spinner-preloader.gif') {
+		fs.readFile('./public/images/lg.ajax-spinner-preloader.gif', (err, fileContent) => {
+		    if (err) {
+			console.log('Error 4');
+		    } else {
+			res.writeHead(200, {'Content-Type': 'image/jpg'});
+			res.end(fileContent);
+		    }
+		});	
+	    } else {
+		var splittedUrl = req.url.split('/');
+		if (splittedUrl[1] === 'getpic') {
+		    // Check user input
+		    if (isInputLegal(getUserInput(req.url))) {
+			// Find photo from link given, give it to user and store request in db.
+			getPhoto(getUserInput(req.url), res, collection, storeRequestInDb);
 		    } else {
 			res.statusCode = 404;
-			res.end('error');
+			res.end('Wrong link');			    
 		    }
-		}
-	    } else if (req.method === 'POST') {
-		if (req.url === '/getpic/') {
-		    console.log('Got post request at /getpic/');
-		    console.log(req); // Where is the body??
-		    //getPhoto('https://www.google.com', res);
 		} else {
 		    res.statusCode = 404;
 		    res.end('error');
 		}
+	    }
+	} else if (req.method === 'POST') {
+	    if (req.url === '/getpic/') {
+		console.log('Got post request at /getpic/');
+		console.log(req); // Where is the body??
+		//getPhoto('https://www.google.com', res);
 	    } else {
 		res.statusCode = 404;
 		res.end('error');
 	    }
-	});
-	
-	server.listen(port, () => {
-	    console.log(`Server running at port ${port}`);
-	});
+	} else {
+	    res.statusCode = 404;
+	    res.end('error');
+	}
+    });
+    
+    server.listen(port, () => {
+	console.log(`Server running at port ${port}`);
     });
 });
 
-// Get an url and an http server response object, the user ip, a db collection, and
+// Get an url and an http server response object, a db collection, and
 // a callback to insert the request in the db collection.
 // End the server response with the content retrieved from the url.
-function getPhoto (url, handle, ip, dbCollection, callback) {
+function getPhoto (url, handle, dbCollection, callback) {
     https.get(url, (res) => {
 	const { statusCode } = res;
 	const contentType = res.headers['content-type'];
@@ -125,7 +122,6 @@ function getPhoto (url, handle, ip, dbCollection, callback) {
 
 		// Insert user request into db
 		callback({
-		    ip: ip,
 		    requested: url,
 		    found: originalPhoto,
 		    date: new Date().toLocaleString()
