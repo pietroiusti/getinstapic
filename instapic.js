643f29921eb26ffc3359068aa2f1e13fa1024e6a
glptrst <giulio.pietroiusti@gmail.com>
2020-05-21 20:07:55 +0200

    Remove isInputLegal function.

--- instapic.js
+++ instapic.js
@@ -1,83 +1,71 @@
 const https = require('https');
 
 module.exports = {
-    getPage: getPage,
-    redirectUserToPic: redirectUserToPic,
-    getPhotoUrl: getPhotoUrl,
-    getUserInput: getUserInput,
-    isInputLegal: isInputLegal
+  getPage: getPage,
+  redirectUserToPic: redirectUserToPic,
+  getPhotoUrl: getPhotoUrl,
+  getUserInput: getUserInput,
 };
 
 // return a promise to page
 function getPage (url) {
-    return new Promise( (resolve, reject) => {
-	https.get(url, (res) => {
-	    const { statusCode } = res;
-	    const contentType = res.headers['content-type'];
-	    let error;
-	    if (statusCode !== 200) {
-		error = new Error('Request Failed.\n' +
-				  `Status Code: ${statusCode}`);
-	    }
-	    let rawData = '';
-	    res.on('data', (chunk) => {
-		rawData += chunk;
-	    });
-	    res.on('end', () => {
-		try {
-		    resolve(rawData);
-		} catch (e) {
-		    reject(e);
-		}
-	    });
-	}).on('error', (e) => {
-	    reject(e);
-	});
+  return new Promise( (resolve, reject) => {
+    https.get(url, (res) => {
+      const { statusCode } = res;
+      const contentType = res.headers['content-type'];
+      let error;
+      if (statusCode !== 200) {
+	error = new Error('Request Failed.\n' +
+			  `Status Code: ${statusCode}`);
+      }
+      let rawData = '';
+      res.on('data', (chunk) => {
+	rawData += chunk;
+      });
+      res.on('end', () => {
+	try {
+	  resolve(rawData);
+	} catch (e) {
+	  reject(e);
+	}
+      });
+    }).on('error', (e) => {
+      reject(e);
     });
+  });
 };
 
 // Redirect user to picture url; take page url and a response object
 async function redirectUserToPic(pageUrl, res) {
-    try {
-	let page = await getPage(getUserInput(pageUrl));
-	let picture = getPhotoUrl(page);
-	// redirect to picture
-	res.statusCode = 302;
-	res.setHeader('Location', picture);
-	res.end();
-    } catch(e) {
-	console.log(e);
-    }
+  try {
+    let page = await getPage(getUserInput(pageUrl));
+    let picture = getPhotoUrl(page);
+    // redirect to picture
+    res.statusCode = 302;
+    res.setHeader('Location', picture);
+    res.end();
+  } catch(e) {
+    console.log(e);
+  }
 }
 
 // Find url photo within the html/js code of the page.
 function getPhotoUrl (page) {
-    let regex = /"og:image.+content=".+"/.exec(page);
-    regex = /http.+\"/.exec(regex);
+  let regex = /"og:image.+content=".+"/.exec(page);
+  regex = /http.+\"/.exec(regex);
 
-    return regex[0].substring(0, regex[0].length-1);
+  return regex[0].substring(0, regex[0].length-1);
 };
 
 // Return user input from url of get request
 function getUserInput(url) {
-    // split url using '/'
-    var splittedUrl = url.split('/');
-    // Leave only link part in the array
-    splittedUrl.splice(0,2);
-    // Join with '/'
-    var userInput = splittedUrl.join('/');
-    // remove '?link=' from the beginning of string
-    userInput = decodeURIComponent(userInput.slice(6, userInput.length));
-    return userInput;
+  // split url using '/'
+  var splittedUrl = url.split('/');
+  // Leave only link part in the array
+  splittedUrl.splice(0,2);
+  // Join with '/'
+  var userInput = splittedUrl.join('/');
+  // remove '?link=' from the beginning of string
+  userInput = decodeURIComponent(userInput.slice(6, userInput.length));
+  return userInput;
 }
-
-// Check whether the input from the user is the right kind of url
-function isInputLegal(input) {
-    let rightPrefix = "https://www.instagram.com/";
-    let actualPrefix = input.slice(0,26);
-    if (actualPrefix === rightPrefix) {
-	return true;
-    } else {
-	return false;
-    }
-};
--- server.js
+++ server.js
@@ -1,60 +1,61 @@
 "use strict";
 const http = require('http');
-const https = require('https');
 const fs = require('fs');
 const instapic = require('./instapic.js');
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    // Add error listener
-    req.on('error', (err) => {
-	console.log(err);
-	res.statusCode = 400;
-	res.end();
-    });
+  // Add error listener
+  req.on('error', (err) => {
+    console.log(err);
+    res.statusCode = 400;
+    res.end();
+  });
 
-    if (req.method === 'GET') {
-	if (req.url === '/') {
-	    fs.readFile('./public/homepage.html', (err, fileContent) => {
-		if (err) {
-		    console.log('Error 1');
-		} else {
-		    res.writeHead(200, {'Content-Type': 'text/html'});
-		    res.end(fileContent);
-		}
-	    });
-	} else if (req.url === '/cover.css') {
-	    fs.readFile('./public/css/cover.css', (err, fileContent) => {
-		if (err) {
-		    console.log('Error 2');
-		} else {
-		    res.writeHead(200, {'Content-Type': 'text/css'});
-		    res.end(fileContent);
-		}
-	    });
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      fs.readFile('./public/homepage.html', (err, fileContent) => {
+	if (err) {
+	  console.log('Error 1');
 	} else {
-	    var splittedUrl = req.url.split('/');
-	    if (splittedUrl[1] === 'getpic') {
-		// Check user input
-		if (instapic.isInputLegal(instapic.getUserInput(req.url))) {
-		    // Redirect user to picture
-		    instapic.redirectUserToPic(req.url, res);
-		} else {
-		    res.statusCode = 404;
-		    res.end('Wrong link');			    
-		}
-	    } else {
-		res.statusCode = 404;
-		res.end('error');
-	    }
+	  res.writeHead(200, {'Content-Type': 'text/html'});
+	  res.end(fileContent);
 	}
+      });
+    } else if (req.url === '/cover.css') {
+      fs.readFile('./public/css/cover.css', (err, fileContent) => {
+	if (err) {
+	  console.log('Error 2');
+	} else {
+	  res.writeHead(200, {'Content-Type': 'text/css'});
+	  res.end(fileContent);
+	}
+      });
     } else {
+      var splittedUrl = req.url.split('/');
+      if (splittedUrl[1] === 'getpic') {
+	// Check user input
+	if (instapic.getUserInput(req.url).slice(0, 26)
+	    ===
+	    "https://www.instagram.com/") {
+	  // Redirect user to picture
+	  instapic.redirectUserToPic(req.url, res);
+	} else {
+	  res.statusCode = 404;
+	  res.end('Wrong link');			    
+	}
+      } else {
 	res.statusCode = 404;
 	res.end('error');
+      }
     }
+  } else {
+    res.statusCode = 404;
+    res.end('error');
+  }
 });
 
 server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
+  console.log(`Server running at port ${port}`);
 });
