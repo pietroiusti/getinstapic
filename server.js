fb5020f87676339dc8d426835383ad3a361a24af
glptrst <giulio.pietroiusti@gmail.com>
2020-05-22 19:58:20 +0200

    Put everything in a single js file.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 "use strict";
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
-const instapic = require('./instapic.js');
 
 const port = process.env.PORT || 3000;
 
@@ -36,11 +36,11 @@ const server = http.createServer((req, res) => {
       var splittedUrl = req.url.split('/');
       if (splittedUrl[1] === 'getpic') {
 	// Check user input
-	if (instapic.getUserInput(req.url).slice(0, 26)
+	if (getUserInput(req.url).slice(0, 26)
 	    ===
 	    "https://www.instagram.com/") {
 	  // Redirect user to picture
-	  instapic.redirectUserToPic(req.url, res);
+	  redirectUserToPic(req.url, res);
 	} else {
 	  res.statusCode = 404;
 	  res.end('Wrong link');			    
@@ -59,3 +59,66 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
+
+// return a promise to page
+function getPage (url) {
+  return new Promise( (resolve, reject) => {
+    https.get(url, (res) => {
+      const { statusCode } = res;
+      const contentType = res.headers['content-type'];
+      let error;
+      if (statusCode !== 200) {
+	error = new Error('Request Failed.\n' +
+			  `Status Code: ${statusCode}`);
+      }
+      let rawData = '';
+      res.on('data', (chunk) => {
+	rawData += chunk;
+      });
+      res.on('end', () => {
+	try {
+	  resolve(rawData);
+	} catch (e) {
+	  reject(e);
+	}
+      });
+    }).on('error', (e) => {
+      reject(e);
+    });
+  });
+};
+
+// Redirect user to picture url; take page url and a response object
+async function redirectUserToPic(pageUrl, res) {
+  try {
+    let page = await getPage(getUserInput(pageUrl));
+    let picture = getPhotoUrl(page);
+    // redirect to picture
+    res.statusCode = 302;
+    res.setHeader('Location', picture);
+    res.end();
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+// Find url photo within the html/js code of the page.
+function getPhotoUrl (page) {
+  let regex = /"og:image.+content=".+"/.exec(page);
+  regex = /http.+\"/.exec(regex);
+
+  return regex[0].substring(0, regex[0].length-1);
+};
+
+// Return user input from url of get request
+function getUserInput(url) {
+  // split url using '/'
+  var splittedUrl = url.split('/');
+  // Leave only link part in the array
+  splittedUrl.splice(0,2);
+  // Join with '/'
+  var userInput = splittedUrl.join('/');
+  // remove '?link=' from the beginning of string
+  userInput = decodeURIComponent(userInput.slice(6, userInput.length));
+  return userInput;
+}
